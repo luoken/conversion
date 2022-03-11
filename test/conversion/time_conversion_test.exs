ee67550e7a8ac294296e27c0ff2ddcd326ea4908
ken <ken@vody.com>
2022-03-10 17:39:15 -0800

    updates time conversion tests

--- test/conversion/time_conversion_test.exs
+++ test/conversion/time_conversion_test.exs
@@ -1,30 +1,30 @@
-defmodule ExMsTest do
+defmodule Conversion.TimeConversionTest do
   use ExUnit.Case
-  alias Conversion
+  alias Conversion.TimeConversion
 
-  describe "to_ms/2" do
-    test "to_ms second" do
-      assert 2_000 = Conversion.to_ms(2, :seconds)
+  describe "convert from milliseconds" do
+    test "convert milliseconds to second" do
+      assert 2_000 = TimeConversion.convert(2, :milliseconds, :seconds)
     end
 
-    test "to_ms minute" do
-      assert 180_000 = Conversion.to_ms(3, :minutes)
+    test "ms to minute" do
+      assert 180_000 = TimeConversion.convert(3, :milliseconds, :minutes)
     end
 
-    test "to_ms hour" do
-      assert 21_600_000 = Conversion.to_ms(6, :hours)
+    test "ms to hour" do
+    assert 21_600_000 = TimeConversion.convert(6, :milliseconds, :hours)
     end
 
-    test "to_ms day" do
-      assert 172_800_000 = Conversion.to_ms(2, :days)
+    test "ms to day" do
+      assert 172_800_000 = TimeConversion.convert(2, :milliseconds, :days)
     end
 
-    test "to_ms week" do
-      assert 3_024_000_000 = Conversion.to_ms(5, :weeks)
+    test "ms to week" do
+      assert 3_024_000_000 = TimeConversion.convert(5, :milliseconds, :weeks)
     end
 
-    test "to_ms year" do
-      3.15_576e10 = Conversion.to_ms(1, :years)
+    test "ms to year" do
+      3.15_576e10 = TimeConversion.convert(1, :milliseconds, :years)
     end
   end
 end
