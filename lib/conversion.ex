4c438ff0e9dcdb253fb56f00057d1ba7b2b8dc9f
ken <ken@vody.com>
2022-04-06 22:42:55 -0700

    removes defdelegate from conversion.ex

--- lib/conversion.ex
+++ lib/conversion.ex
@@ -2,8 +2,4 @@ defmodule Conversion do
   @moduledoc """
   Elixir library for converting one unit of measurement to another.
   """
-
-  defdelegate convert(interval, to, from), to: Conversion.Time
-
-  defdelegate convert(temp, to, from), to: Conversion.Temperature
 end
